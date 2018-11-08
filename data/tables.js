a6df51329aab21e80e17a8eff24aea3163fcd595
Your Name <salil_bhise@yahoo.com>
2018-11-07 20:21:45 -0600

    changed reservationID to 1,2, 3, for bootstrap dropdown

--- data/tables.js
+++ data/tables.js
@@ -4,21 +4,21 @@ const tables = [
         name: 'Gordon Ramsay',
         phoneNumber: '555-867-5309',
         email: 'itsraw@gmail.com',
-        reservationID: 'cheframsay18',
+        reservationID: '1',
     },
     {
         routeName: 'Table 2',
         name: 'Andrew Zimmern',
         phoneNumber: '555-123-4567',
         email: 'bizzarefoods@yahoo.com',
-        reservationID: 'azimmern19',
+        reservationID: '2',
     },
     {
         routeName: 'Table 3',
         name: 'Michael Mina',
         phoneNumber: '555-345-9876',
         email: 'mmina69@me.com',
-        reservationID: 'mikemina69',
+        reservationID: '3',
     }
 ];
 
