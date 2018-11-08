2f2b7f0944bd222beb6382e05357c4c45a99b549
Your Name <salil_bhise@yahoo.com>
2018-11-07 19:57:30 -0600

    missed a couple commas, but added them in

--- data/tables.js
+++ data/tables.js
@@ -1,20 +1,20 @@
 const tables = [
     {
-        routeName: 'Table 1'
+        routeName: 'Table 1',
         name: 'Gordon Ramsay',
         phoneNumber: '555-867-5309',
         email: 'itsraw@gmail.com',
         reservationID: 'cheframsay18',
     },
     {
-        routeName: 'Table 2'
+        routeName: 'Table 2',
         name: 'Andrew Zimmern',
         phoneNumber: '555-123-4567',
         email: 'bizzarefoods@yahoo.com',
         reservationID: 'azimmern19',
     },
     {
-        routeName: 'Table 3'
+        routeName: 'Table 3',
         name: 'Michael Mina',
         phoneNumber: '555-345-9876',
         email: 'mmina69@me.com',
